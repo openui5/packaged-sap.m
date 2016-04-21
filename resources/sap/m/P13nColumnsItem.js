dc9bd5d8d6fdeb326dee0d9d51830b753ff1c6cb
OpenUI5 <openui5@sap.com>
2016-04-21 17:50:17 +0200

    1.36.8

--- resources/sap/m/P13nColumnsItem.js
+++ resources/sap/m/P13nColumnsItem.js
@@ -17,7 +17,7 @@ sap.ui.define([
 	 * @param {object} [mSettings] initial settings for the new control
 	 * @class Type for <code>columnsItems</code> aggregation in P13nColumnsPanel control.
 	 * @extends sap.ui.core.Item
-	 * @version 1.36.7
+	 * @version 1.36.8
 	 * @constructor
 	 * @author SAP SE
 	 * @public
