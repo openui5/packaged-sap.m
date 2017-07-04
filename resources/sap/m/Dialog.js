76d9025d65df7794f2c97e082850888129863d33
OpenUI5 <openui5@sap.com>
2017-07-04 12:54:42 +0200

    1.28.47

--- resources/sap/m/Dialog.js
+++ resources/sap/m/Dialog.js
@@ -24,7 +24,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Toolbar', '
 	 * @implements sap.ui.core.PopupInterface
 	 *
 	 * @author SAP SE
-	 * @version 1.28.46
+	 * @version 1.28.47
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/m/UploadCollection.js
+++ resources/sap/m/UploadCollection.js
@@ -19,7 +19,7 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './Dialog', './library', 'sa
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.28.46
+	 * @version 1.28.47
 	 *
 	 * @constructor
 	 * @public
